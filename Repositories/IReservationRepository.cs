010146dc5ebf5c759b72029e2d97f861e5ec5685
Andy16-Js <andyorozcocastro98@gmail.com>
2026-04-15 17:29:13 -0600

    feat: add delete and get by id interface contracts in reservation repository

--- Repositories/IReservationRepository.cs
+++ Repositories/IReservationRepository.cs
@@ -6,4 +6,5 @@ public interface IReservationRepository
 {
   Task<Reservation> AddAsync(Reservation reservation);
   Task<List<Reservation>> GetByClassroomAndDateAsync(Guid classroomId, DateOnly date);
+  Task DeleteAsync(Guid id);
 }
